c9b3671e531b682a2f9965659e77091a9cb7291f
Victor Hugo B <victorhugobeppler@gmail.com>
2023-01-22 23:58:18 -0300

    Build(Configuração do CORS)
    Foi feito uma configuração basica do cors para  que o front consiga consultar as apis do back-end

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import cors from "@fastify/cors";
 
 const app = fastify();
 const prisma = new PrismaClient();
+
+app.register(cors,{
+  origin: "*",
+});
 
 app.get("/consult/habit", async () => {
   const habit = await prisma.habit.findMany();
   return habit;
 });
+
 app.listen({ port: 3000 }, () => {
   console.log("Server is running on port 3000");
 });
