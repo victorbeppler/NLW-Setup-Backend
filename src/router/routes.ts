b2c67274726c53e5c7b5554918347505bb13805d
Victor Hugo B <victorhugobeppler@gmail.com>
2023-01-24 22:58:33 -0300

    Build(Criação de novas rotas) Foram criadas duas novas rotas sendo elas: - Toggle Habit - Responsável por marcar o hábito concluido ou remover o mesmo. - Summary - Responsável por retornar o resumo de dias e suas informações.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -71,4 +71,79 @@ export async function appRoutes(app: FastifyInstance) {
       .status(200)
       .send({ possibleHabits: possibleHabitsRequest, days: completedHabits });
   });
+
+  app.patch("/habits/:id/toggle", async (request, response) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+      response.status(200).send("A marcação foi removida com sucesso!");
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+      response.status(201).send("A marcação foi criada com sucesso!");
+    }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH 
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE 
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unizepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+    return summary;
+  });
 }
