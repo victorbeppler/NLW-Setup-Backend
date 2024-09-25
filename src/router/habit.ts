1fa3a16bd91ae898f034c0f27650b0cfed6ca84d
victorhugb <victorhugob@hypergroup.com.br>
2024-09-25 00:21:19 -0300

    Refactor summary query in habit router

--- src/router/habit.ts
+++ src/router/habit.ts
@@ -126,26 +126,25 @@ router.patch("/habits/:id/toggle", async (req, res) => {
 
 router.get("/summary", async (req, res) => {
   const summary = await prisma.$queryRaw`
-      SELECT 
-        D.id, 
-        D.date,
-        (
-          SELECT 
-            cast(count(*) as float)
-          FROM day_habits DH
-          WHERE DH.day_id = D.id
-        ) as completed,
-        (
-          SELECT
-            cast(count(*) as float)
-          FROM habit_week_days HDW
-          JOIN habits H
-            ON H.id = HDW.habit_id
-          WHERE
-            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
-            AND H.created_at <= D.date
-        ) as amount
-      FROM days D
+SELECT 
+  D.id, 
+  D.date,
+  (
+    SELECT 
+      cast(count(*) as float)
+    FROM day_habits DH
+    WHERE DH.day_id = D.id
+  ) as completed,
+  (
+    SELECT
+      cast(count(*) as float)
+    FROM habit_week_days HDW
+    JOIN habits H ON H.id = HDW.habit_id
+    WHERE
+      HDW.week_day = DAYOFWEEK(D.date) - 1
+      AND H.created_at <= D.date
+  ) as amount
+FROM days D;
     `;
 
   res.status(200).json(summary);
